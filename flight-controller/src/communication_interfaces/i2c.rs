fe87ce35827dd983b3e5024ceea9ceffeffcedde
Manuel Baez <manuelbaezbatista@gmail.com>
2024-03-18 14:22:49 -0400

    Revert back to 400khz, another approach to improve performance could be to poll the gyro data and accelerometer data at differentintervals

--- flight-controller/src/communication_interfaces/i2c.rs
+++ flight-controller/src/communication_interfaces/i2c.rs
@@ -1,5 +1,4 @@
 use embedded_hal::i2c::I2c;
-use esp_idf_svc::hal::interrupt::InterruptType;
 use esp_idf_svc::hal::prelude::*;
 use esp_idf_svc::hal::{
     i2c::{I2cConfig, I2cDriver, I2cError},
@@ -13,7 +12,7 @@ pub fn get_i2c_driver(peripherals: &mut Peripherals) -> I2cDriver<'static> {
     let sda = unsafe { peripherals.pins.gpio21.clone_unchecked() };
     let scl = unsafe { peripherals.pins.gpio22.clone_unchecked() };
     let i2c = unsafe { peripherals.i2c0.clone_unchecked() };
-    let config = I2cConfig::new().baudrate(1000.kHz().into());
+    let config = I2cConfig::new().baudrate(400.kHz().into());
 
     I2cDriver::new(i2c, sda, scl, &config).unwrap()
 }
